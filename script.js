1ff8b71ed5d8806799effa0babdc85c5dc29a61f
Vana Niranjan <113905277+Vananiranjanarao@users.noreply.github.com>
2023-05-08 12:22:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+//window.alert("if you want to perform multiplication then click ok");
+const num1=Math.ceil(Math.random()*10);
+const num2=Math.ceil(Math.random()*10);
+//console.log(num1);
+const questionE1=document.getElementById("question");
+const formE1=document.getElementById(form);
+
+const inputE1=document.getElementById("input");
+const scoreE1=document.getElementById("score");
+let score= JSON.parse( localStorage.getItem("score"));
+
+questionE1.innerText=`what is ${num1} multiply by ${num2}?`
+
+const correctAns=num1*num2;
+formE1.addEventListener("submit",()=>{
+const userAns= +inputE1.value
+if(userAns===correctAns){
+    score++
+    updateLocalStorage()
+   // console.log(score)
+}
+else{
+    score--
+    //console.log(score)
+    updateLocalStorage()
+}
+
+//console.log(userAns,typeof(userAns));
+});
+function updateLocalStorage(){
+    localStorage.setItem("score",JSON.stringify(score));
+}
+
+
+
+
+
+
